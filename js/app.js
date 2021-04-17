403eaa0a4ee0544011056857abd49e842ad5adb3
ChaosAbyss <merkulov.vadim1@yandex.ru>
2021-04-17 10:42:30 +0300

    Small structure changes.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Task grid
 const openPopupButtons = document.querySelectorAll('[data-popup-target]')
 const closePopupButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
@@ -20,15 +21,33 @@ function openPopup(popup) {
   if (popup == null) return
   popup.classList.add('active')
   overlay.classList.add('active')
+  const startButton = document.getElementById('start-button') 
+  startTask(startButton)
 }
 
 function closePopup(popup) {
   if (popup == null) return
   popup.classList.remove('active')
   overlay.classList.remove('active')
+  const finishButton = document.getElementById('finish-button') 
+  finishTask(finishButton)
 }
 
 function reply_click(clicked_id) {
   title = document.getElementById("popup__title")
   title.innerHTML = clicked_id + " задание"
+}
+
+function startTask(startButton) {
+  startButton.addEventListener('click', () => {
+    const infoBox = document.getElementById('info__box')
+    infoBox.style.display = 'none'
+  })
+}
+
+function finishTask(finishButton) {
+  finishButton.addEventListener('click', () => {
+    const infoBox = document.getElementById('info__box')
+    infoBox.style.display = 'initial'
+  })
 }
