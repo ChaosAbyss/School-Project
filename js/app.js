f25e307b627e373c6f218ad181bab7cddb181d03
ChaosAbyss <merkulov.vadim1@yandex.ru>
2021-04-24 23:07:31 +0300

    Added text to the quiz popup. Small changes.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const closePopupButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
 // Grid elements
-openPopupButtons.forEach(button => {
+openPopupButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const popup = document.querySelector(button.dataset.popupTarget)
     openPopup(popup)
@@ -12,7 +12,7 @@ openPopupButtons.forEach(button => {
 })
 
 // Popup close button
-closePopupButtons.forEach(button => {
+closePopupButtons.forEach((button) => {
   button.addEventListener('click', () => {
     const popup = button.closest('.popup')
     closePopup(popup)
@@ -29,34 +29,41 @@ function replyClick(clicked_id) {
 
 function openPopup(popup) {
   if (popup == null) return
-  popup.style.visibility = 'visible';
-  popup.style.opacity = '1';
+  popup.style.visibility = 'visible'
+  popup.style.opacity = '1'
   popup.classList.add('active')
   overlay.classList.add('active')
-  const startButton = document.getElementById('start-button') 
+  const startButton = document.getElementById('start-button')
   startTask(popup, startButton)
 }
 
 function closePopup(popup) {
   if (popup == null) return
-  popup.style.visibility = 'hidden';
+  popup.style.visibility = 'hidden'
   popup.style.opacity = '0'
   popup.classList.remove('active')
   overlay.classList.remove('active')
   const infoBox = document.getElementById('info__box')
-  const startButton = document.getElementById('start-button') 
-  setTimeout(() => { infoBox.style.display = 'initial'; startButton.style.display = 'initial';}, 300);
+  const startButton = document.getElementById('start-button')
+  setTimeout(() => {
+    infoBox.style.display = 'initial'
+    startButton.style.display = 'initial'
+  }, 300)
 }
 
 function startTask(popup, startButton) {
   startButton.addEventListener('click', () => {
     const quiz = document.getElementById('quiz')
-    const finishButton = document.getElementById('finish-button') 
+    const finishButton = document.getElementById('finish-button')
     popup.classList.remove('active')
+    popup.style.visibility = 'hidden'
+    popup.style.opacity = '0'
     setTimeout(() => {
-    quiz.classList.add('active');
-    quiz.style.visibility = 'visible';
-    quiz.style.opacity = '1'}, 350);
+      quiz.classList.add('active')
+      quiz.style.visibility = 'visible'
+      quiz.style.opacity = '1'
+      showQuestions()
+    }, 350)
     finishTask(quiz, finishButton)
   })
 }
@@ -65,8 +72,23 @@ function finishTask(quiz, finishButton) {
   if (finishButton == null) return
   finishButton.addEventListener('click', () => {
     quiz.classList.remove('active')
-    quiz.style.visibility = 'hidden' 
+    quiz.style.visibility = 'hidden'
     quiz.style.opacity = '0'
     overlay.classList.remove('active')
   })
+}
+
+function showQuestions() {
+  const questionNumber = document.getElementById('task__number')
+  const questionText = document.getElementById('task__text')
+  const questionImage = document.getElementById('task__image')
+  const questionQuestion = document.getElementById('task__question')
+  let title = questions_math[0].number
+  let text = questions_math[0].task
+  let image = questions_math[0].image
+  let question = questions_math[0].question
+  questionNumber.innerHTML = number
+  questionText.innerHTML = text
+  questionImage.innerHTML = image
+  questionQuestion.innerHTML = question
 }
